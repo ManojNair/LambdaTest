344379aef8ca30f72eeabf7f5d1680ff31c607a1
ManojNair <Manoj.Nair@outlook.com>
2017-02-04 00:00:59 +1100

    Initial Commit

---
+++ Invoke-LMGreetingFunction.Tests.ps1
@@ -0,0 +1,9 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Invoke-LMGreetingFunction" {
+    It "Prints `"Hello World`"" {
+        Invoke-LMGreetingFunction | Should Be "`"Hello World`""
+    }
+}
---
+++ Invoke-LMGreetingFunction.ps1
@@ -0,0 +1,7 @@
+﻿function Invoke-LMGreetingFunction
+{
+	$output = Invoke-LMFunction -FunctionName greetings -Payload "{`"name`":`"World`"}" -Region ap-southeast-2
+	$writer = [System.IO.StreamReader]::New($output.Payload)
+	$writer.ReadToEnd()
+
+}
